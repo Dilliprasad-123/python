759e958c6d3309ced4c393372b6f2fe763eccb7c
Dilliprasad-123 <padmakshidilliprasad@gmail.com>
2026-08-31 22:03:38 +0530

    basics tasks

---
+++ basictask.py
@@ -0,0 +1,61 @@
+
+# 1.	Print your name, age, and college name.
+# 2.	Take input from the user and print a welcome message
+
+
+# name=input("Enter your name: ")
+# print("Hello, " + name)
+
+# school=input("Enter your school name: ")
+#  print("Hello, " + school)
+
+# age=input("Enter your age: ")
+# print("Hello, " + name + " your school name is " + school + " and your age is " + age)
+
+# ======>O/P
+""" Enter your name: Dilli
+Enter your school name: bhashyam
+Enter your age: 23
+Hello, Dilli your school name is bhashyam and your age is 23  """
+
+# =====================================================================
+
+# 3.	Create a program that adds two numbers.
+
+
+# a=input("Enter your number: ")
+# b=input("Enter your number: ")
+# c=float(a)+float(b)
+# # c=int(a)+int(b)
+# print(c)
+
+"""
+Enter your number: 23
+Enter your number: 34.0
+57.0
+"""
+# ===================================================================
+
+# 4.	Convert a string number "25" into integer and add 5 to it.
+# num_str="25"
+# num_int=int(num_str)
+# result=num_int+5
+# print(result)
+
+"""O/P: 30"""
+
+# ==================================================================
+
+"""# 5.	Print the data type of these values:
+o	"Hello"
+o	10
+o	10.5
+o	True
+"""
+print(type("Hello"))
+print(type(10))
+print(type(10.5))
+print(type(True))   
+
+
+
