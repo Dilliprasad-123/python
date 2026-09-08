fc76b6a6eaf88e7d935304e15ffc2f961fa5a92c
Dilliprasad-123 <padmakshidilliprasad@gmail.com>
2026-09-08 17:05:46 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,33 @@
+# print("Hello, World!")
+# print('hello, world!')
+
+"""
+Hello, World!
+hello, world!
+"""
+
+# S="  Hello, WoRld! "
+# print(S)                     #Hello, World!
+
+
+# print(S[0])                  #H
+
+# # print(S[13])               #IndexError: string index out of range
+
+# print(S[-1])                 #!
+# print(len(S))                #13
+# print(S.upper())             #HELLO, WORLD!
+# print(S.lower())             #hello, world!
+# print(S.strip())             #Hello, World!
+# print(S.replace("H", "J"))   #Jello, World!
+# print(S[0:5])                #Hello
+# print(S[2:5])                #llo
+# print(S[2:])                 #llo, WoRld!
+# print(S[:5])                 #Hello
+# print("Hello" in S)          #True
+# print("hi" in S)             #False
+
+a= "Hello"
+b= "World"
+c= a + " " + b
+print(c)                       #Hello World
---
+++ variables.py
@@ -0,0 +1,67 @@
+# Variable Declaration
+
+# a,b,c="Dilli","Prasad","Dilli Prasad"
+# print(a)    
+# print(b)
+# print(c)
+
+"""  Assigning Multitple Values
+O/P
+Dilli
+Prasad
+Dilli Prasad
+
+"""
+
+
+# age=30
+# print(age)
+# Age=age+10
+# print(Age)
+# AGE=Age+10
+# print(AGE)
+
+
+"""O/P:case sensitive
+30
+40
+50
+ 
+"""
+
+
+# x=10
+# print(x)
+
+# x="dilli"
+# print(x)
+
+# print("hi " + x)
+# print("hi " ,x ," how are you")
+
+"""
+10
+dilli
+hi dilli
+hi  dilli  how are you"""
+
+
+a=b=c=10
+print(a)
+print(b)
+print(c)
+
+
+
+
+# ===============Operators========================
+#  Arithmetic Operators
+# a=10
+# b=5
+# print(a+b)  # Addition
+# print(a-b)  # Subtraction
+# print(a*b)  # Multiplication
+# print(a/b)  # Division
+# print(a//b) # Floor Division
+# print(a%b)  # Modulus
+# print(a**b) # Exponentiation
