5ef71e7ddd12c25a12069081452633f2d858be85
Dilliprasad-123 <padmakshidilliprasad@gmail.com>
2026-09-10 10:27:05 +0530

    Add files via upload

---
+++ conditionalstm.py
@@ -0,0 +1,23 @@
+
+# ============ conditional statements ==============
+
+# 1. If statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# 2. If-else statement
+y = 3
+if y % 2 == 0:
+    print("y is even")
+else:
+    print("y is odd")
+
+# 3. If-elif-else statement
+z = 7
+if z < 0:
+    print("z is negative")
+elif z == 0:
+    print("z is zero")
+else:
+    print("z is positive")
